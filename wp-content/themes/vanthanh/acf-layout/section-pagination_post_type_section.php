0aaf564cc9e7c8a5de05f76ff149b07614499eb1
hiep <hiep.dao@leximit.com>
2021-12-20 12:13:24 +0700

    tin tuc

--- wp-content/themes/vanthanh/acf-layout/section-pagination_post_type_section.php
+++ wp-content/themes/vanthanh/acf-layout/section-pagination_post_type_section.php
@@ -19,23 +19,25 @@ if ( isset( $args ) && $args ) { ?>
 					$paged        = get_query_var( 'paged' ) ? ( get_query_var( 'paged' ) - 1 ) : 0;
 					$postsPerPage = $args['posts_per_page'] ?: 12;
 					$postOffset   = $paged * $postsPerPage;
-					$args_posts = array(
+					$args_posts   = array(
 						'post_type'      => $args['post_type'],
 						'offset'         => $postOffset,
 						'posts_per_page' => $postsPerPage,
 						'orderby'        => 'publish_date',
 						'order'          => 'DESC'
 					);
-					$postsList  = new WP_Query( $args_posts );
+					$postsList    = new WP_Query( $args_posts );
 
-					if ( $postsList->have_posts() ) :
-						while ( $postsList->have_posts() ) :
-							$postsList->the_post();
-							get_template_part( 'template-parts/grid_item', $args['post_type'] );
-						endwhile;
-						?>
+					if ( $postsList->have_posts() ) { ?>
+                        <div class="listing-news-wrap">
+							<?php while ( $postsList->have_posts() ) :
+								$postsList->the_post();
+								get_template_part( 'template-parts/grid_item', $args['post_type'] );
+							endwhile;
+							?>
+                        </div>
 
-                        <div class="pagination">
+                        <div class="pagination _right">
 							<?php
 							echo paginate_links( array(
 								'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
@@ -55,9 +57,8 @@ if ( isset( $args ) && $args ) { ?>
 							?>
                         </div>
 
-						<?php
-						wp_reset_postdata();
-					endif;
+						<?php wp_reset_postdata(); ?>
+					<?php }
 
 				}
 				?>
